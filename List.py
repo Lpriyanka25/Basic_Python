a3c52ecb796542829f0335ece3e50e89ade08178
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-10-28 22:32:12 +0530

    Update List.py

--- List.py
+++ List.py
@@ -1,3 +1,4 @@
+#EXAMPLE 1
 # Creating a list
 fruits = ["apple", "banana", "cherry"]
 
@@ -21,3 +22,21 @@ print(fruits)  # Output: ['apple', 'mango', 'cherry', 'orange']
 # Removing the last element
 fruits.pop()
 print(fruits)  # Output: ['apple', 'mango', 'cherry']
+
+#EXAMPLE 2
+# Creating a new list with squares of numbers
+squares = [x**2 for x in range(5)]
+print(squares)  # Output: [0, 1, 4, 9, 16]
+
+numbers = [4, 2, 8, 6, 7]
+
+# Sorting
+numbers.sort()
+print(numbers)  # Output: [2, 4, 6, 7, 8]
+
+# Reversing
+numbers.reverse()
+print(numbers)  # Output: [8, 7, 6, 4, 2]
+
+# Counting occurrences
+print(numbers.count(8))  # Output: 1
