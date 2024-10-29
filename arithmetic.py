d695ff2eac3a346e8e22aceb2b3763505b04886f
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-10-29 21:07:20 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+a = 5
+b = 3
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
