004a5e32a8fc4b8af71a52c77fb5fc074c761f1a
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-10-30 22:52:07 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,3 +1,4 @@
+#EXAMPLE 1
 a = 5
 b = 3
 sum = a + b
@@ -8,3 +9,11 @@ print("Sum:", sum)
 print("Difference:", difference)
 print("Product:", product)
 print("Quotient:", quotient)
+
+#EXAMPLE 2
+#first take inupt from user
+#addition of number
+num1 = input("Enter number:")
+num2 = input("Enter number:")
+num3 = int(num1) + int(num2)
+print("sum is :",num3)
