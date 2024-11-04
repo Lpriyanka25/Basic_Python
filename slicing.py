73f1ca3e31a0e5887f2c1cde18e94d74da37f31f
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-11-04 22:21:48 +0530

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -1,7 +1,5 @@
 months = ["January", "February", "March", "April", "May", "June", "July"]
-
 print(months[0:6])
-
 print(months[1:5])
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -10,3 +8,7 @@ numbers[:5]
 numbers[5:]    
 numbers[1:8:2] 
 numbers[::-1]
+
+text = "Hello, World!"
+text[7:12]      
+text[::-1]       
