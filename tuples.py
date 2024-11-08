9084edd1ddba76461dd6dfc510eacd3ca03e0581
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-11-08 23:12:13 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# Empty tuple
+empty_tuple = ()
+
+# Tuple with elements
+my_tuple = (1, 2, 3, "apple", "banana")
+
+# Access first element
+print(my_tuple[0])  # Output: 1
+
+# Access last element
+print(my_tuple[-1])  # Output: "banana"
+
+a, b, c, d, e = my_tuple
+print(a)  # Output: 1
+print(d)  # Output: "apple"
+
