553117e554e5b879a2967a5b56f6cb17af2d0794
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-11-14 23:51:19 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -5,3 +5,11 @@ print("Sorted list:", sorted_numbers)
 numbers = [5, 2, 9, 1, 5, 6]
 numbers.sort()
 print("Sorted list:", numbers)
+
+# Using sorted()
+sorted_numbers_desc = sorted(numbers, reverse=True)
+print("Descending sorted list:", sorted_numbers_desc)
+
+# Using sort() method
+numbers.sort(reverse=True)
+print("Descending sorted list:", numbers)
