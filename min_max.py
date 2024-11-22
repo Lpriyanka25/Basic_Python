9311fe57f4a9afdbdc9e6ed57cb9fae5d98835cc
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-11-22 22:51:12 +0530

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -20,3 +20,19 @@ try:
         print("You did not enter any numbers.")
 except ValueError:
     print("Invalid input! Please enter only numbers separated by spaces.")
+
+#example 2
+def find_min_max(numbers):
+    if not numbers:
+        return "The list is empty!"
+    
+    min_num = min(numbers)
+    max_num = max(numbers)
+    return min_num, max_num
+
+# Example usage
+numbers = [10, 20, 5, 35, 50, -10]
+min_num, max_num = find_min_max(numbers)
+
+print(f"Minimum number: {min_num}")
+print(f"Maximum number: {max_num}")
