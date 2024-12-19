cf041501a1cabfc3acb4e0906e16d56c45981843
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-12-19 23:20:36 +0530

    Create TowerBreakers.py

---
+++ TowerBreakers.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    # If towers have height 1, no moves can be made, so Player 2 wins
+    if m == 1:
+        return 2
+    # If number of towers is even, Player 2 can mirror Player 1's moves and win
+    elif n % 2 == 0:
+        return 2
+    # Otherwise, Player 1 wins
+    else:
+        return 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
